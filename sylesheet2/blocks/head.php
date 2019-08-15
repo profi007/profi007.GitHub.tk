b5c2b7a26bfebf1e98b8a2d5758efca0751bfbbb
дмитрий <kiler.delentor@gmail.com>
2019-08-15 22:57:48 +0300

    Revert "Add files via upload"
    This reverts commit d2015fc1348cd6a1e1c30a95add8544707202809.

--- sylesheet2/blocks/head.php
+++
@@ -1,3 +0,0 @@
-<meta charset="utf-8">
-<Link href="/css/Style.css" rel="sylesheet"> 
-<title><?php title ?></title>
--- sylesheet2/blocks/rightcol.php
+++
@@ -1,7 +0,0 @@
-<div id="rightcol">
-<div class="baner">
-<img src="/img/baner.jpg" alt="банер" title="банер">
-</div>
-<div class="baner_2">
-<img src="/img/baner_2.jpg" alt="банер_2" title="банер_2">
-</div>
